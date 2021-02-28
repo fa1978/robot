5f3291d668bd57f29f6461848ea05ed68666e369
fa1978 <admndba78@gmail.com>
2021-02-28 04:29:31 +0100

    Updates

---
+++ precache-manifest.b71966fcdbdf298ac9dcac8e6c8935d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f41b7e3d635a2a1fa1fbd657529618",
+    "url": "/robot/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robot/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f1efa900b00621da3736",
+    "url": "/robot/static/css/main.fac1d41d.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robot/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "f1efa900b00621da3736",
+    "url": "/robot/static/js/main.62134dd8.chunk.js"
+  },
+  {
+    "revision": "7b897043843942566e08",
+    "url": "/robot/static/js/runtime~main.55db2e1d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
